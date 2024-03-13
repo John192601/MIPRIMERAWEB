81debaa9cdb3719e563b2f4f6f30a3a1b02f1be0
John192601 <163221351+John192601@users.noreply.github.com>
2024-03-13 18:56:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+//alert("Hola este es mi Javascript");
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+  navBar = document.querySelector(".nav2");
+  navBar.classList.toggle("active");
+};
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  parrafo.innerHTML = "";
+
+  if (nombre.value.length < 4) {
+    warnings += `El nombre debe contener más de 4 carcateres`;
+    valido = false;
+  }
+
+  if (!valido) {
+    parrafo.innerHTML = warnings;
+  } else {
+    parrafo.innerHTML = "Enviado";
+  }
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+  } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  }
+});
